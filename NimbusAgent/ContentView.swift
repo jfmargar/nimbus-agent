e1262a47d6416370daededb040f05063aa00fb1d
Julián Marqués García <julian.marques@dinamicarea.com>
2026-02-26 09:15:51 +0100

    Initial Commit

---
+++ NimbusAgent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NimbusAgent
+//
+//  Created by Julián Marqués García on 26/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NimbusAgent/NimbusAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  NimbusAgentApp.swift
+//  NimbusAgent
+//
+//  Created by Julián Marqués García on 26/2/26.
+//
+
+import SwiftUI
+
+@main
+struct NimbusAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
